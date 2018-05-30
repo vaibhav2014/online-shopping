ca237d8f75d45b788f49b9464185e228b0cc8dcf
ivpz <blackaccn@gmail.com>
2018-05-30 07:08:00 +0530

    project seetup completed

---
+++ shoppingbackend/src/main/java/net/krn/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.krn.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
